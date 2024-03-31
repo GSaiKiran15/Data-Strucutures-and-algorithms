e8c4bb5112436c7a579dd6a065ad4853548f9612
Sai Kiran Ganesh Kumar <ganeshsaikiran7@gmail.com>
2024-03-30 20:40:58 -0600

    Jump Game Completed

--- Leetcode/Medium/jumpgame.py
+++ Leetcode/Medium/jumpgame.py
@@ -1,11 +1,26 @@
-nums = [3,2,1,0,4]
-i = 0
-while i <= len(nums):
-    if i == len(nums)-1:
-        print(True)
-        break
-    if nums[i] <= 0 and i != len(nums)-1:
-        print(False)
-        break
-    i += nums[i]
-print(False)
+nums = [2,3,1,1,4]
+
+#Best Solution
+
+goal = len(nums)-1
+
+for i in range(len(nums)-1, -1, -1):
+    print(i)
+    if i + nums[i] >= goal:
+        goal = i
+print(True if goal == 0 else False)
+
+# My solution
+# if len(nums) > 1:
+#     i = -2
+#     goal = -1
+#     while i > -len(nums):
+#         if i + nums[i] >= goal:
+#             goal = i
+#         i -= 1
+#     if goal == -len(nums):
+#         print(True)
+#     else:
+#         print(False)
+# else:
+#     print(False)
